9c59dc48b9ad66391603f2fbb56d6f326cf06678
Alexander Lehner <alexlehner86@gmail.com>
2022-01-08 15:27:24 +0000

    some tweaks

--- app.js
+++ app.js
@@ -6,5 +6,12 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
+app.get('/teapot', (req, res) => {
+	res
+		.status(418)
+		.set('X-Oida', 'is des org')
+		.send('I\'m a teapot!');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
