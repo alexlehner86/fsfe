8c2050c0eeeb132525ff76da3d48cb10423e10c3
Alexander Lehner <xander_lehner@gmx.at>
2022-01-08 17:43:55 +0100

    fix typo

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const path = require=('path');
+const path = require('path');
 
 const app = express();
 const port = 3000;
+
+app.use(
+	express.static(path.join(__dirname, '../html'))
+);
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../html/index.html'));
